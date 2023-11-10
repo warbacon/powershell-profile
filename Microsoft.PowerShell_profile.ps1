0a1daedb687be800b44b8046309464aea59b14f6
Warbacon <joaquinguerratocino@gmail.com>
2023-11-10 22:11:33 +0100

    Remove WT_CONFIG

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -15,8 +15,6 @@ function Get-PublicIP {
     (Invoke-WebRequest http://ifconfig.me/ip).Content
 }
 
-$WT_CONFIG = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
 # APPEARANCE -------------------------------------------------------------------
 $PSStyle.FileInfo.Directory="$($PSStyle.Bold)$($PSStyle.Foreground.Blue)"
 Set-PSReadLineOption -Colors @{
