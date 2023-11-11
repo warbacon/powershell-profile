a27805e0e43bcdc8b79c118b8ccb36dd86ebabc5
Warbacon <joaquinguerratocino@gmail.com>
2023-11-11 16:47:19 +0100

    Switch to ohmyposh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -46,42 +46,43 @@ if (Test-CommandExists nvim) {
 }
 
 # PROMPT -----------------------------------------------------------------------
-if ((Test-CommandExists starship) -and (-not $STARSHIP_DISABLED)) {
-    ## CONFIGURE AND START STARSHIP
-    $ENV:STARSHIP_CONFIG = "$HOME\Documents\PowerShell\starship.toml"
-    $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
-
-    Invoke-Expression (&starship init powershell)
-
-    function Invoke-Starship-PreCommand {
-        if ($PWD.Path.StartsWith($HOME)) {
-            $host.ui.RawUI.WindowTitle = "~$($PWD.Path.Substring($HOME.Length))"
-        } else {
-            $host.ui.RawUI.WindowTitle = "$PWD"
-        }
-    }
-} else {
-    ## SIMPLE AND FAST PROMPT
-    function prompt {
-        $success = $Global:?
-
-        $currentPath = $PWD.Path
-        if ($currentPath.StartsWith($HOME)) {
-            $currentPath = "~" + $currentPath.Substring($HOME.Length)
-        }
-        $Host.UI.RawUI.WindowTitle = $currentPath
-
-        $prompt += "`n"
-        $prompt += "$($PSStyle.Bold)$($PSStyle.Foreground.Cyan)$currentPath$($PSStyle.Reset)"
-        $prompt += "`n"
-
-        if ($success) {
-            $prompt += "$($PSStyle.Foreground.Green)❯"
-        } else {
-            $prompt += "$($PSStyle.Foreground.Red)❯"
-        }
-        $prompt += "$($PSStyle.Reset) "
-
-        return $prompt
-    }
-}
+oh-my-posh init pwsh --config "$HOME/Documents/Powershell/thunder.json" | Invoke-Expression
+# if ((Test-CommandExists starship) -and (-not $STARSHIP_DISABLED)) {
+#     ## CONFIGURE AND START STARSHIP
+#     $ENV:STARSHIP_CONFIG = "$HOME\Documents\PowerShell\starship.toml"
+#     $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
+#
+#     Invoke-Expression (&starship init powershell)
+#
+#     function Invoke-Starship-PreCommand {
+#         if ($PWD.Path.StartsWith($HOME)) {
+#             $host.ui.RawUI.WindowTitle = "~$($PWD.Path.Substring($HOME.Length))"
+#         } else {
+#             $host.ui.RawUI.WindowTitle = "$PWD"
+#         }
+#     }
+# } else {
+#     ## SIMPLE AND FAST PROMPT
+#     function prompt {
+#         $success = $Global:?
+#
+#         $currentPath = $PWD.Path
+#         if ($currentPath.StartsWith($HOME)) {
+#             $currentPath = "~" + $currentPath.Substring($HOME.Length)
+#         }
+#         $Host.UI.RawUI.WindowTitle = $currentPath
+#
+#         $prompt += "`n"
+#         $prompt += "$($PSStyle.Bold)$($PSStyle.Foreground.Cyan)$currentPath$($PSStyle.Reset)"
+#         $prompt += "`n"
+#
+#         if ($success) {
+#             $prompt += "$($PSStyle.Foreground.Green)❯"
+#         } else {
+#             $prompt += "$($PSStyle.Foreground.Red)❯"
+#         }
+#         $prompt += "$($PSStyle.Reset) "
+#
+#         return $prompt
+#     }
+# }
