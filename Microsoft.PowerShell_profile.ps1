922a51efa32f4ad891803abe5eee6a17d2d9b408
Warbacon <joaquinguerratocino@gmail.com>
2024-01-11 22:10:54 +0100

    feat: duplicate tab support in Windows Terminal

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -32,6 +32,12 @@ if (Get-Command -ErrorAction SilentlyContinue -Name "nvim.exe") {
 # STARSHIP ---------------------------------------------------------------------
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     function Invoke-Starship-PreCommand {
+        $loc = $executionContext.SessionState.Path.CurrentLocation
+        $prompt = "$([char]27)]9;12$([char]7)"
+        if ($loc.Provider.Name -eq "FileSystem") {
+            $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+        }
+        $host.ui.Write($prompt)
         $host.ui.RawUI.WindowTitle = "$pwd".Replace("$HOME", "~")
     }
 
