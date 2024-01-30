b30f2259b718baa5bac4e744bc3cf2d1b8a509dd
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-01-30 19:28:54 +0100

    change: remove duplicate tab support
    It's just bloat

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -28,11 +28,6 @@ if (Get-Command lazygit -ErrorAction SilentlyContinue) {
 # STARSHIP --------------------------------------------------------------------
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     function Invoke-Starship-PreCommand {
-        $loc = $executionContext.SessionState.Path.CurrentLocation
-        $prompt = "$([char]27)]9;12$([char]7)"
-        if ($loc.Provider.Name -eq "FileSystem") {
-            $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-        }
         $host.ui.Write($prompt)
         $host.ui.RawUI.WindowTitle = "$pwd".Replace("$HOME", "~")
     }
@@ -41,16 +36,4 @@ if (Get-Command starship -ErrorAction SilentlyContinue) {
     $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
 
     Invoke-Expression (&starship init powershell --print-full-init | Out-String)
-}
-else {
-    function prompt {
-        $loc = $executionContext.SessionState.Path.CurrentLocation;
-
-        $out = ""
-        if ($loc.Provider.Name -eq "FileSystem") {
-            $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-        }
-        $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-        return $out
-    }
 }
