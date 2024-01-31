c9302a546dc3024522936cba914aaa9e29e3ef0d
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-01-31 20:31:06 +0100

    Refactor code

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,39 +1,39 @@
 # APPEARANCE ------------------------------------------------------------------
 $PSStyle.FileInfo.Directory = "$($PSStyle.Bold)$($PSstyle.Foreground.Blue)"
-
-# PSREADLINE ------------------------------------------------------------------
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
-Set-PSReadLineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
-Set-PSReadLineKeyHandler -Key Ctrl+Backspace -Function BackwardDeleteWord
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -Colors @{
     Parameter        = "Blue"
     Operator         = "Blue"
     InlinePrediction = "DarkGray"
 }
 
+# KEYBINDINGS -----------------------------------------------------------------
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key Ctrl+Backspace -Function BackwardDeleteWord
+Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
 # ALIASES ---------------------------------------------------------------------
 function .. { Set-Location .. }
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name ex -Value explorer.exe
 
-if (Get-Command lazygit -ErrorAction SilentlyContinue) {
+if (Get-Command lazygit.exe -ErrorAction SilentlyContinue) {
     Set-Alias -Name lg -Value lazygit.exe
 }
 
 # STARSHIP --------------------------------------------------------------------
-if (Get-Command starship -ErrorAction SilentlyContinue) {
+if (Get-Command starship.exe -ErrorAction SilentlyContinue) {
     function Invoke-Starship-PreCommand {
-        $host.ui.Write($prompt)
-        $host.ui.RawUI.WindowTitle = "$pwd".Replace("$HOME", "~")
+        $Host.UI.Write($prompt)
+        $Host.UI.RawUI.WindowTitle = "$pwd".Replace("$HOME", "~")
     }
 
-    $ENV:STARSHIP_CONFIG = "$HOME\Documents\Powershell\starship.toml"
-    $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
+    $Env:STARSHIP_CONFIG = "$HOME\Documents\Powershell\starship.toml"
+    $Env:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
 
     Invoke-Expression (&starship init powershell --print-full-init | Out-String)
 }
